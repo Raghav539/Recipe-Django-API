ecc45347fd9c03df7e2edfa85865b3e2e75e45a5
Raghav539 <raghavgaurpandit@gmail.com>
2026-03-27 10:42:56 +0530

    Install drf-spectacular for auto documentation of schema

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core'
+    'rest_framework',
+    'drf-spectacular',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': ' drf_spectacular.openapi.AutoSchema',
+}
