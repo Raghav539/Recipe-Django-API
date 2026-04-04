e6236fcd86f591302f6f1b032e22b4216d786df1
Raghav539 <raghavgaurpandit@gmail.com>
2026-04-04 21:36:16 +0530

    All Tags Api Done

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,15 +2,65 @@
 Serializers for the recipe app.
 """
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import Recipe, Tag
+
+
+
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for recipe tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            if not attrs.get('name'):
+                raise serializers.ValidationError('Name is required for creating a tag.')
+        return attrs
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe objects."""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'description', 'link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'description', 'link','tags']
         read_only_fields = ['id']
+
+    def _get_or_create_tags(self, tags_data, recipe):
+        """Handle getting or creating tags as needed."""
+        auth_user = self.context['request'].user
+        for tag_data in tags_data:
+            tag, created = Tag.objects.get_or_create(**tag_data, user=auth_user)
+            recipe.tags.add(tag)
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags_data = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags_data, recipe)
+        return recipe
+
+
+    def update(self, instance, validated_data):
+        """Update a recipe."""
+        tags_data = validated_data.pop('tags', None)
+        if tags_data is not None:
+
+            instance.tags.clear()
+            self._get_or_create_tags(tags_data, instance)
+
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
     def validate(self,attrs):
         request = self.context.get('request')
@@ -25,3 +75,4 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,10 +3,10 @@ Views for the recipe API.
 
 """
 
-from rest_framework import viewsets
+from rest_framework import (viewsets,mixins)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Recipe
+from core.models import (Recipe,Tag)
 from recipe import serializers
 
 
@@ -31,4 +31,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 viewsets.GenericViewSet):
+    """Manage tags in the database."""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag."""
         serializer.save(user=self.request.user)
