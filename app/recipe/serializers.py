63a6d353342005ba928f71d69217582f5f62e500
Raghav539 <raghavgaurpandit@gmail.com>
2026-04-04 11:06:19 +0530

    Add Title Validation in Create Recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,6 +12,13 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'description', 'link']
         read_only_fields = ['id']
 
+    def validate(self,attrs):
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            if not attrs.get('title'):
+                raise serializers.ValidationError('Title is required for creating a recipe.')
+        return attrs
+
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
